04d91410bc76be368cec2a30edf7c3f86912040f
Samuel Jonas <105826679+SamuelEternal@users.noreply.github.com>
2023-09-10 21:26:25 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,9 +1,11 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import javax.swing.text.StyledEditorKit.BoldAction;
+
 class Quarto{
-    int num_Quarto;
-    Boolean status_quarto = false;
+    protected int num_Quarto;
+    protected Boolean status_quarto = false;
 
     public Quarto(int num_Quarto, boolean status_quarto){
         this.num_Quarto = num_Quarto;
@@ -27,18 +29,6 @@ class Quarto{
 
 }
 
-class Suite extends Quarto{
-
-
-}
-
-class King extends Quarto{
-
-}
-
-class Queen extends King{
-
-}
 
 class Employee{
     String name_Employee;
@@ -57,16 +47,22 @@ class Employee{
 class Cliente{
     String nome_Cliente;
     String cpf_Cliente;
+    String password;
 
-    public Cliente(String nome_Cliente, String cpf_Cliente){
+    public Cliente(String nome_Cliente, String cpf_Cliente, String password){
         this.nome_Cliente = nome_Cliente;
         this.cpf_Cliente = cpf_Cliente;
+        this.password = password;
     }
-    public String get_cpf(String nome_Cliente){
+    public String getCpf(String nome_Cliente){
         return cpf_Cliente;
     }
-    public String get_Nome(String cpf_Cliente){
+    public String getNome(String cpf_Cliente){
         return nome_Cliente;
+    
+    }
+    public String getPassword(String nome_Cliente){
+        return password;
     }
     public String reservar_Quarto(int num_Quarto, String cpf_Cliente){
         //terminar função de reservas chamando outra função de quarto
@@ -74,8 +70,23 @@ class Cliente{
 
 
     }
-    
+    private boolean checkPassword(String nomeCliente, String password) {
+    String storedPassword = getPassword(nomeCliente);
+    return storedPassword != null && storedPassword.equals(password);
+    }
+    }
+
+class Normal{
+
 }
+class Gold{
+
+}
+class Premium{
+
+}
+
+
 
 public class Main{
     public static void main(String[] args){

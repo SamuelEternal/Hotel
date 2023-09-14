f3446aa6c6d37cab1dd054da02c7e5a16074377a
Liedson1 <Liedson146@gmail.com>
2023-09-13 22:54:47 -0300

    só complementei algumas coisas

--- Main.java
+++ Main.java
@@ -1,17 +1,14 @@
-//import java.util.ArrayList;
-//import java.util.Scanner; 
-
-public class Main{
-    public static void main(String[] args){
-        Quarto quarto204 = new Quarto(204, false, "");
-        Quarto quarto205 = new Quarto(205, false, "");
+public class Main {
+    public static void main(String[] args) {
+        Quarto quarto204 = new Quarto(204, false, ""); // Add the missing arguments
+        Quarto quarto205 = new Quarto(205, false, ""); // Add the missing arguments
         Cliente cliente1 = new Cliente("José", "senha123");
         Cliente cliente2 = new Cliente("Pedro", "batata");
         cliente1.reservarQuarto(quarto204, cliente1.getNome(), "senha123");
-        cliente2.reservarQuarto(quarto204, cliente2.getNome(), "batata");
-       
-      
-    
-    
+        cliente2.reservarQuarto(quarto205, cliente2.getNome(), "batata");
+
+
+        System.out.println("Status of Quarto 204: " + quarto204.getStatus());
+        System.out.println("Status of Quarto 205: " + quarto205.getStatus());
     }
 }

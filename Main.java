7482a3920212abfbb8a195f019cdb5f976c6d99b
Liedson1 <Liedson146@gmail.com>
2023-09-14 01:56:53 -0300

    ^^^^

--- Main.java
+++ Main.java
@@ -1,20 +1,20 @@
 
 public class Main {
     public static void main(String[] args) {
-        // Create instances of QuartoComum and QuartoPremium
+
         QuartoComum quartoComum = new QuartoComum(204);
         QuartoPremium quartoPremium = new QuartoPremium(204);
 
-        // Create clients
+
         Cliente cliente1 = new Cliente("José", "senha123");
         Cliente cliente2 = new Cliente("Pedro", "batata");
 
-        // Reserve rooms using the Cliente class
+
         cliente1.reservarQuarto(quartoComum, cliente1.getNome(), "senha123", "Quarto Comum");
         cliente2.reservarQuarto(quartoPremium, cliente2.getNome(), "batata", "Quarto Premium");
 
 
-        // Print the status of the rooms
+
         System.out.println("Status of Quarto 204: " + quartoComum.getStatus());
         System.out.println("Status of Quarto 205: " + quartoPremium.getStatus());
     }
